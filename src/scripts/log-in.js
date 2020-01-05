11a027df9777fab9f57d52b27704b5878dd70231
spektr001 <sanek777.28.03.2000@gmail.com>
2020-01-05 20:54:08 +0200

    "messanger"  design status: in progress

---
+++ src/scripts/log-in.js
@@ -0,0 +1,14 @@
+
+ class Warning {
+     constructor(selector) {
+         this.$el = document.querySelector(selector)
+     }
+
+     hide() {
+         this.$el.style.display = 'none'
+     }
+
+     show() {
+         this.$el.classList.add()
+     }
+ }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path')
 
 module.exports = {
-    entry: './src/main.js',
+    entry: './src/main.js', 
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     }
 }
+
